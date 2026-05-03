50fa2a942d9096878e284f31f325b62937b4dbe0
CaioAlvescvl <caiovictorcvl11@gmail.com>
2026-05-03 03:57:08 -0300

    adição das opções cadastro,listar e sair

--- python.py
+++ python.py
@@ -1,24 +1,47 @@
 aluno = [
   {
     "nome": "márcio",
-    "matrícula": 230261,
+    "matricula": 230261,
     "curso": "Análise e Desenvolvimento de Sistemas",
     "idade": 21,
     "nota": 10
   },
   {
     "nome": "paulo",
-    "matrícula": 230262,
+    "matricula": 230262,
     "curso": "gastronomia",
     "idade": 28,
     "nota": 7.0
   },
   {
     "nome": "gabriela",
-    "matrícula": 230263,
+    "matricula": 230263,
     "curso": "direito",
     "idade": 24,
     "nota": 8.5
   }
 ]
-print(aluno)
+while True:
+  print("1 - Cadastrar aluno")
+  print("2 - Listar alunos")
+  print("0 - Sair")
+
+  opcao = input("Escolha uma opcao: ")
+
+  if opcao == "1":
+    nome_novo_aluno = input("Digite o nome do novo aluno: ")
+    matricula_novo_aluno = int(input("Digite a matricula do novo aluno: "))
+    curso_novo_aluno = input("Digite o curso do novo aluno: ")
+    idade_novo_aluno = int(input("Digite a idade do novo aluno: "))
+    nota_novo_aluno = float(input("Digite a nota do novo aluno: "))
+
+    novo_aluno = {
+      "nome": nome_novo_aluno,
+      "matricula": matricula_novo_aluno,
+      "curso": curso_novo_aluno,
+      "idade": idade_novo_aluno,
+      "nota": nota_novo_aluno
+    }
+
+    aluno.append(novo_aluno)
+    print("aluno cadastrado com sucesso!")
